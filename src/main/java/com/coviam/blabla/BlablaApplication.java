7aa7ef7ac9005f255483b5d7ad798e0bf2bff037
Coviam <coviam@Coviams-MacBook-Pro.local>
2017-06-03 11:30:23 +0530

    first commit

---
+++ src/main/java/com/coviam/blabla/BlablaApplication.java
@@ -0,0 +1,12 @@
+package com.coviam.blabla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlablaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlablaApplication.class, args);
+	}
+}
