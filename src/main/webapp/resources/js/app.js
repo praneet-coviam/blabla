b85aa16b37632d92a8544b6fb473b967e46c43ab
Coviam <coviam@Coviams-MacBook-Pro-2.local>
2017-06-03 17:54:26 +0530

    carousal added

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,9 +1,1 @@
-var myApp = angular.module('myApp',[]);
-		myApp.controller('myCtrl', function($scope) {
-          // $scope.myMovie=function(){
-          //     $scope.movieSelected=true;
-          //     userRepository.getMyMovie().success(function(data) {
-          //      $scope.data = data;
-          //   });
-          // }
-		});
+$('.carousel.carousel-slider').carousel({fullWidth: true});
