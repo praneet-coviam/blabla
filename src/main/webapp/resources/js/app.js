6e618afdc9b01a51264891ba4c8e30a8166bcd42
Coviam <coviam@Coviams-MacBook-Pro-2.local>
2017-06-05 10:39:03 +0530

    small_product view added & responsible sidenavbar added

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,1 +1,16 @@
-$('.carousel.carousel-slider').carousel({fullWidth: true});
+
+$(document).ready(function () {
+    $('.carousel.carousel-slider').carousel({ fullWidth: true });
+    $('.modal').modal()
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.carousel').carousel({
+        padding: 200
+    });
+    autoplay()
+    function autoplay() {
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 4500);
+    }
+});
+
+
