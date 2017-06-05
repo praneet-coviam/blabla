072bf735ecb3d8708205040ad7b9f2a1f8e4eb28
Ankit Sharan <ankit@Ankits-MacBook-Pro.local>
2017-06-05 10:43:02 +0530

    controllers and templates created

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,1 +1,24 @@
-$('.carousel.carousel-slider').carousel({fullWidth: true});
+var myApp = angular.module('myApp', ["ngRoute"]);
+myApp.controller('myCtrl', function ($scope) {	
+ 
+});
+myApp.config(function($routeProvider) {
+    $routeProvider
+    .when("/home", {
+        templateUrl : "Templates/home.html",
+        controller: 'homeController'
+    })
+    .when("/Product", {
+        templateUrl : "product.html"
+    })
+    .when("/list", {
+        templateUrl : "list.html"
+    })
+    .when("/kart", {
+        templateUrl : "kart.html"
+    });
+});
+myApp.controller('homeController', function($scope) {
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
+	
+});
