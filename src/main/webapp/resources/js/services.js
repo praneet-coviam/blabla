0db3b6ddf9dba7221358dbb201d084ec1a98f5d7
Sai Krishna <coviam@Coviams-MacBook-Pro.local>
2017-06-06 09:48:02 +0530

    Merging UI

--- src/main/webapp/resources/js/services.js
+++ src/main/webapp/resources/js/services.js
@@ -3,7 +3,7 @@ var myApp = angular.module('myApp');
 myApp.factory('userRepository',function ($http){
    return {
         getByCategory:function(){
-           var url="http://localhost:8080/category/tablet";
+           var url="http://localhost:8080/category/phone";
            return $http.get(url);
        }
    };
